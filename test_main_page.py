ee600ed2a889d9dce8e60750986c682f2defb65e
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2022-07-22 12:05:06 +0300

    test go to login page is done

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -14,3 +14,11 @@ def test_quest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_quest_can_go_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
