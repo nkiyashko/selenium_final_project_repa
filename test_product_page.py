9d6459f877230832b345c35a10bc90c438bec17a
nkiyashko <71321251+nkiyashko@users.noreply.github.com>
2022-08-16 14:41:34 +0300

    4.4 solve

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,12 @@
 import pytest
 from .pages.product_page import ProductPage
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('url', ["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo"
+                                 "=newYear",
+                                 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo"
+                                 "=newYear2019"])
+def test_guest_can_add_product_to_basket(browser, url):
+    link = url
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
